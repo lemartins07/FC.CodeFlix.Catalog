75c580a9f786281972d3787b3c35e24487a60470
Leandro Martins <leandro.martins.85@gmail.com>
2024-07-11 22:46:45 -0300

    Iniciando a implementação da Category

--- tests/FC.CodeFlix.Catalog.UnitTests/Domain/Entity/Category/CategoryTests.cs
+++ tests/FC.CodeFlix.Catalog.UnitTests/Domain/Entity/Category/CategoryTests.cs
@@ -16,11 +16,51 @@ public class CategoryTests
         };
 
         // Act
+        var datetimeBefore = DateTime.Now;
+
         var category = new DomainEntity.Category(validData.Name, validData.Description);
+
+        var datetimeAfter = DateTime.Now;
 
         // Assert
         Assert.NotNull(category);
         Assert.Equal(validData.Name, category.Name);
         Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > datetimeBefore);
+        Assert.True(category.CreatedAt < datetimeAfter);
+        Assert.True(category.IsActive);
+    }
+
+    [Theory(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        // Arrange
+        var validData = new
+        {
+            Name = "Category Name",
+            Description = "Category Description"
+        };
+
+        // Act
+        var datetimeBefore = DateTime.Now;
+
+        var category = new DomainEntity.Category(validData.Name, validData.Description, isActive);
+
+        var datetimeAfter = DateTime.Now;
+
+        // Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > datetimeBefore);
+        Assert.True(category.CreatedAt < datetimeAfter);
+        Assert.Equal(category.IsActive, category.IsActive);
     }
 }

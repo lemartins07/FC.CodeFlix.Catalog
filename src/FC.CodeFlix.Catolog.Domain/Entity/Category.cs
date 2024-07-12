30a028c09ad9b5e5b3917abbcb9b3d48b6dbf8e1
Leandro Martins <leandro.martins.85@gmail.com>
2024-07-11 22:32:03 -0300

    Adicionar arquivos de projeto.

---
+++ src/FC.CodeFlix.Catolog.Domain/Entity/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace FC.CodeFlix.Catalog.Domain.Entity;
+
+public class Category
+{
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+
+    public Category(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
---
+++ tests/FC.CodeFlix.Catalog.UnitTests/Domain/Entity/Category/CategoryTests.cs
@@ -0,0 +1,26 @@
+﻿using DomainEntity = FC.CodeFlix.Catalog.Domain.Entity;
+namespace FC.Catalog.UnitTests.Domain.Entities.Category;
+
+
+public class CategoryTests
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void Instantiate()
+    {
+        // Arrange
+        var validData = new
+        {
+            Name = "Category Name",
+            Description = "Category Description"
+        };
+
+        // Act
+        var category = new DomainEntity.Category(validData.Name, validData.Description);
+
+        // Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+    }
+}

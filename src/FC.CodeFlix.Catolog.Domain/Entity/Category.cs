47e373dac046c149450cdca399e66ac7920968da
Leandro Martins <leandro.martins.85@gmail.com>
2024-07-15 22:30:04 -0300

    refatorando com seedwork

--- src/FC.CodeFlix.Catolog.Domain/Entity/Category.cs
+++ src/FC.CodeFlix.Catolog.Domain/Entity/Category.cs
@@ -1,18 +1,18 @@
 ﻿using FC.CodeFlix.Catolog.Domain.Exceptions;
+using FC.CodeFlix.Catolog.Domain.SeedWork;
 
 namespace FC.CodeFlix.Catalog.Domain.Entity;
 
-public class Category
-{
-    public Guid Id { get; private set; }
+public class Category : AggregateRoot
+{   
     public string Name { get; private set; }
     public string Description { get; private set; }
     public bool IsActive { get; private set; }
     public DateTime CreatedAt { get; private set; }
 
-    public Category(string name, string description, bool isActive = true)
+    public Category(string name, string description, bool isActive = true) : base()
     {
-        Id = Guid.NewGuid();
+        
         Name = name;
         Description = description;
         IsActive = isActive;
---
+++ src/FC.CodeFlix.Catolog.Domain/SeedWork/AggregateRoot.cs
@@ -0,0 +1,6 @@
+﻿namespace FC.CodeFlix.Catolog.Domain.SeedWork;
+
+public abstract class AggregateRoot: Entity
+{
+    protected AggregateRoot() : base() { }
+}
